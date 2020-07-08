db7374628b1c24ce80d82ef6ef12ca03c1e8e12a
Stian have <stianhave@hotmail.com>
2020-07-08 23:40:40 +0200

    Cleaned code and added more model properties

--- web_scraper/Controllers/WebScraperController.cs
+++ web_scraper/Controllers/WebScraperController.cs
@@ -20,7 +20,6 @@ namespace web_scraper.Controllers {
 		private readonly string websiteUrl = "https://www.finn.no/job/fulltime/search.html?filters=&occupation=0.23&occupation=1.23.244&occupation=1.23.83&page=1&sort=1";
 		private readonly ILogger<WebScraperController> _logger;
 
-		// Constructor
 		public WebScraperController(ILogger<WebScraperController> logger) {
 			_logger = logger;
 		}
@@ -42,28 +41,32 @@ namespace web_scraper.Controllers {
 					// Create a container object
 					JobModel advert = new JobModel();
 
-					// Use regex to get all the numbers from this string
 					var position = row.QuerySelector(".ads__unit__content__keys");
+					var info = row.QuerySelector(".ads__unit__link");
 					if (position != null) {
+						//Executing this if statement together with the one above causes
+						//NullPointerException
+						if (string.IsNullOrWhiteSpace(position.TextContent)) {
+							advert.Position = info.TextContent;
+						}
 						advert.Position = position.TextContent;
-						/*TODO Remove*/
+						advert.ShortDescription = info.TextContent;
 						Console.WriteLine(advert.Position);
 					} else {
-						advert.Position = "null";
+						advert.Position = info.TextContent;
 					}
 
 					var imageUrl = row.QuerySelector(".img-format__img");
-					if (position != null) {
-						advert.ImageUrl = imageUrl.TextContent;
-					} else {
-						advert.ImageUrl = "";
-					}
+					advert.ImageUrl = imageUrl.GetAttribute("src");
 
-					var link = row.QuerySelector(".ads__unit__link");
-					if (position != null) {
-						advert.AdvertUrl = link.TextContent;
-					} else {
-						advert.AdvertUrl = "";
+					advert.AdvertUrl = "https://finn.no" + info.GetAttribute("href");
+
+					var contentList = row.QuerySelectorAll(".ads__unit__content__list");
+					try {
+						advert.Admissioner = contentList[0].TextContent;
+						advert.NumberOfPositions = contentList[1].TextContent;
+					} catch (Exception e) {
+						Console.WriteLine(e);
 					}
 
 					results.Add(advert);
@@ -100,8 +103,31 @@ namespace web_scraper.Controllers {
 			 * and the await keyword guarantees that nothing else is done
 			 * before that operation is complete.
 			 */
-			await GetPageData(url, adverts);
-
+			adverts = await GetPageData(url, adverts);
+			int stillinger = 0;
+			int NullStillinger = 0;
+			foreach (var jobb in adverts) {
+				if (!string.IsNullOrWhiteSpace(jobb.Position)) {
+					Console.WriteLine("\n");
+					Console.WriteLine($"tittel: {jobb.Position}");
+					Console.WriteLine($"ShortDescription: {jobb.ShortDescription}");
+					Console.WriteLine($"bilde: {jobb.ImageUrl}");
+					Console.WriteLine($"url: {jobb.AdvertUrl}");
+					Console.WriteLine($"positions: {jobb.NumberOfPositions}");
+					Console.WriteLine($"employer: {jobb.Admissioner}");
+					stillinger++;
+				} else {
+					Console.WriteLine("\n@@@@@@@@@@@@@@@@@@@@@@@");
+					Console.WriteLine($"tittel: {jobb.Position}");
+					Console.WriteLine($"ShortDescription: {jobb.ShortDescription}");
+					Console.WriteLine($"positions: {jobb.NumberOfPositions}");
+					Console.WriteLine($"employer: {jobb.Admissioner}");
+					Console.WriteLine("@@@@@@@@@@@@@@@@@@@@@@@");
+					NullStillinger++;
+				}
+			}
+			Console.WriteLine($"\nStillinger: {stillinger}");
+			Console.WriteLine($"\nNull Stillinger: {NullStillinger}");
 			// TODO: Diff the data
 		}
 
