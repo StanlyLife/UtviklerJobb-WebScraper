9459a178e86d8f111dc1ac7eeb501f9c1318df07
Stian have <stianhave@hotmail.com>
2020-07-09 23:14:38 +0200

    Implemented database context

---
+++ web_scraper/Data/WebScraperContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using web_scraper.models;
+using website_scraper.Models;
+
+namespace web_scraper.Data {
+
+	public class WebScraperContext : DbContext {
+		public DbSet<JobAdModel> Jobs { get; set; }
+		public DbSet<JobListingModel> JobListings { get; set; }
+		public DbSet<JobTagsModel> JobTags { get; set; }
+		public DbSet<JobCategoryModel> JobCategories { get; set; }
+
+		public WebScraperContext(DbContextOptions<WebScraperContext> options) : base(options) {
+			Database.EnsureCreated();
+		}
+	}
+}
