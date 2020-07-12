c5b3a454ce21617018160db7fb6752f1fbfeff17
Stian have <stianhave@hotmail.com>
2020-07-13 01:02:33 +0200

    Added new DbSet Added DbSet for Industries

--- web_scraper/Data/WebScraperContext.cs
+++ web_scraper/Data/WebScraperContext.cs
@@ -1,14 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using web_scraper.models;
-using website_scraper.Models;
 
 namespace web_scraper.Data {
 
 	public class WebScraperContext : DbContext {
-		public DbSet<JobAdModel> Jobs { get; set; }
-		public DbSet<JobListingModel> JobListings { get; set; }
+		public DbSet<JobModel> JobListings { get; set; }
 		public DbSet<JobTagsModel> JobTags { get; set; }
 		public DbSet<JobCategoryModel> JobCategories { get; set; }
+		public DbSet<JobIndustryModel> JobIndustry { get; set; }
 
 		public WebScraperContext(DbContextOptions<WebScraperContext> options) : base(options) {
 			Database.EnsureCreated();
