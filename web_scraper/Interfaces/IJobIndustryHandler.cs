03ce6d64a3b507755f130ffd0e11ee56884dacc7
Stian have <stianhave@hotmail.com>
2020-07-13 00:27:07 +0200

    Implemented handler to communicate between controller logic and table

---
+++ web_scraper/Interfaces/IJobIndustryHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using web_scraper.models;
+
+namespace web_scraper.Interfaces {
+
+	public interface IJobIndustryHandler {
+
+		bool SaveChanges();
+
+		/*Purge table*/
+
+		void Purge();
+
+		/*Create*/
+
+		Task<JobIndustryModel> AddJobIndustry(JobIndustryModel industry);
+
+		/*Read*/
+
+		Task<List<JobIndustryModel>> GetJobIndustriesById(string jobId);
+
+		/*Delete*/
+
+		JobIndustryModel DeleteJobIndustryById(JobIndustryModel industry);
+	}
+}
---
+++ web_scraper/Interfaces/Implementations/JobIndustryHandler.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using web_scraper.Data;
+using web_scraper.models;
+using Z.EntityFramework.Plus;
+
+namespace web_scraper.Interfaces.Implementations {
+
+	public class JobIndustryHandler : IJobIndustryHandler {
+		private readonly WebScraperContext db;
+
+		public JobIndustryHandler(WebScraperContext db) {
+			this.db = db;
+		}
+
+		public async Task<JobIndustryModel> AddJobIndustry(JobIndustryModel industry) {
+			await db.JobIndustry.AddAsync(industry);
+			return industry;
+		}
+
+		public JobIndustryModel DeleteJobIndustryById(JobIndustryModel industry) {
+			db.JobIndustry.Remove(industry);
+			return industry;
+		}
+
+		public async Task<List<JobIndustryModel>> GetJobIndustriesById(string jobId) {
+			var query = from entity in db.JobIndustry
+						where entity.JobId.Equals(jobId)
+						select entity;
+			return await query.ToListAsync();
+		}
+
+		public void Purge() {
+			db.JobIndustry.Delete();
+			SaveChanges();
+		}
+
+		public bool SaveChanges() {
+			if (db.SaveChanges() > 0) { return true; }
+			return false;
+		}
+	}
+}
