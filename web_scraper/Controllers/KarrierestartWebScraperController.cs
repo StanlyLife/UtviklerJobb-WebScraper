ecd72f5dbac2373c5ac5f059b8f22b8e47fe91b6
Stian have <stianhave@hotmail.com>
2020-07-21 20:42:45 +0200

    Implemented methods and controller setup for scraping

--- web_scraper/Controllers/KarrierestartWebScraperController.cs
+++ web_scraper/Controllers/KarrierestartWebScraperController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using AngleSharp;
+using AngleSharp.Dom;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using web_scraper.Interfaces;
@@ -13,11 +15,10 @@ namespace web_scraper.Controllers {
 	[ApiController]
 	[Route("ks")]
 	public class KarrierestartWebScraperController : ControllerBase {
-		private readonly string websiteUrl = "https://karrierestart.no/jobb";
+		private readonly string websiteUrl = "https://karrierestart.no/jobb?page=1";
 		private Stopwatch JobAdsTimer = new Stopwatch();
 		private Stopwatch JobListingTimer = new Stopwatch();
 		private readonly int GlobalMaxIteration = 15;
-		private readonly int MaxPagePerQuery = 2;
 		private readonly IJobHandler jobHandler;
 		private readonly IJobCategoryHandler jobCategoryHandler;
 		private readonly IJobTagHandler jobTagHandler;
@@ -30,21 +31,44 @@ namespace web_scraper.Controllers {
 			this.jobTagHandler = jobTagHandler;
 		}
 
-		public async Task<IActionResult> GetAsync() {
+		public async Task<string> GetAsync() {
 			jobTagHandler.Purge();
 			jobHandler.Purge();
 			jobCategoryHandler.Purge();
 			/**/
-			var result = await CheckForUpdates(true);
-			Console.WriteLine($"@@@@@ Finished with {result.Count} results! @@@@@");
+			var result = await CheckForUpdates(websiteUrl);
+			//Console.WriteLine($"@@@@@ Finished with {result.Count} results! @@@@@");
 			/**/
-			Console.WriteLine($"@@@@@ JobAdsTimer Finished after {JobAdsTimer.Elapsed} seconds! @@@@@");
-			Console.WriteLine($"@@@@@ JobAdsTimer Finished after {JobListingTimer.Elapsed} seconds! @@@@@");
+			//Console.WriteLine($"@@@@@ JobAdsTimer Finished after {JobAdsTimer.Elapsed} seconds! @@@@@");
+			//Console.WriteLine($"@@@@@ JobAdsTimer Finished after {JobListingTimer.Elapsed} seconds! @@@@@");
 			/**/
 			return JsonConvert.SerializeObject(result);
 		}
 
-		public async Task<List<JobModel>> CheckForUpdates(bool a) {
+		private async Task<List<JobModel>> CheckForUpdates(string url) {
+			/**/
+			var config = Configuration.Default.WithDefaultLoader();
+			var context = BrowsingContext.New(config);
+			/**/
+
+			await GetJobs(url, context);
+
+			return new List<JobModel>();
+		}
+
+		private async Task<List<JobModel>> GetJobs(string url, IBrowsingContext context) {
+			var document = await context.OpenAsync(url);
+			var jobListings = document.QuerySelectorAll(".featured-wrap");
+
+			foreach (var jobAd in jobListings) {
+				var jobTitle = jobAd.QuerySelector(".title");
+				Console.WriteLine(jobTitle.TextContent);
+			}
+
+			//Check if next page exists
+			//Get Next page url
+			//Recursion
+
 			return new List<JobModel>();
 		}
 	}
