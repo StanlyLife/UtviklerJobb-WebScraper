906deb0dfab2e9aff43593a9c99bf725d80dee60
Stian have <stianhave@hotmail.com>
2020-07-21 20:25:58 +0200

    Initial commit for scrapercontroller This controllers purpose is to scrape karrierestart.no

---
+++ web_scraper/Controllers/KarrierestartWebScraperController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using web_scraper.Interfaces;
+using web_scraper.models;
+
+namespace web_scraper.Controllers {
+
+	[ApiController]
+	[Route("ks")]
+	public class KarrierestartWebScraperController : ControllerBase {
+		private readonly string websiteUrl = "https://karrierestart.no/jobb";
+		private Stopwatch JobAdsTimer = new Stopwatch();
+		private Stopwatch JobListingTimer = new Stopwatch();
+		private readonly int GlobalMaxIteration = 15;
+		private readonly int MaxPagePerQuery = 2;
+		private readonly IJobHandler jobHandler;
+		private readonly IJobCategoryHandler jobCategoryHandler;
+		private readonly IJobTagHandler jobTagHandler;
+		private int iteration = 0;
+		private int currentPage = 1;
+
+		public KarrierestartWebScraperController(IJobHandler jobHandler, IJobCategoryHandler jobCategoryHandler, IJobTagHandler jobTagHandler) {
+			this.jobHandler = jobHandler;
+			this.jobCategoryHandler = jobCategoryHandler;
+			this.jobTagHandler = jobTagHandler;
+		}
+
+		public async Task<IActionResult> GetAsync() {
+			jobTagHandler.Purge();
+			jobHandler.Purge();
+			jobCategoryHandler.Purge();
+			/**/
+			var result = await CheckForUpdates(true);
+			Console.WriteLine($"@@@@@ Finished with {result.Count} results! @@@@@");
+			/**/
+			Console.WriteLine($"@@@@@ JobAdsTimer Finished after {JobAdsTimer.Elapsed} seconds! @@@@@");
+			Console.WriteLine($"@@@@@ JobAdsTimer Finished after {JobListingTimer.Elapsed} seconds! @@@@@");
+			/**/
+			return JsonConvert.SerializeObject(result);
+		}
+
+		public async Task<List<JobModel>> CheckForUpdates(bool a) {
+			return new List<JobModel>();
+		}
+	}
+}
