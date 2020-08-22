03536908cb4d070b3890a2404093a9c8e8542049
Stian have <stianhave@hotmail.com>
2020-08-22 17:16:12 +0200

    Implemented helper methods Methods should check whether the job should be scraped or updated

---
+++ web_scraper/Interfaces/Database/ExistModified.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using web_scraper.Data;
+
+namespace web_scraper.Interfaces.Database {
+
+	public class ExistModified : IExistModified {
+		private readonly WebScraperContext db;
+
+		public ExistModified(WebScraperContext db) {
+			this.db = db;
+		}
+
+		//returns true if job exists in database
+		public bool CheckIfExists(string foreignId) {
+			var query = from entity in db.JobListings
+						where entity.ForeignJobId.Equals(foreignId)
+						select entity;
+			if (query.Any()) {
+				return true;
+			}
+			Debug.WriteLine($"Joblisting with foreignkey {foreignId} - Nav - does not exist in context");
+			return false;
+		}
+
+		//returns true if job description != scraped job description
+		public bool CheckIfModified(string foreignId, string descriptionHtml) {
+			var query = from entity in db.JobListings
+						where
+						entity.ForeignJobId.Equals(foreignId)
+						&&
+						entity.DescriptionHtml.Equals(descriptionHtml)
+						select entity;
+			if (query.Any()) {
+				return false;
+			}
+			Debug.WriteLine($"Joblisting with foreignkey {foreignId} - Nav - is modified, updating!");
+			return true;
+		}
+	}
+}
---
+++ web_scraper/Interfaces/Database/IExistModified.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace web_scraper.Interfaces.Database {
+
+	public interface IExistModified {
+
+		public bool CheckIfExists(string foreignId);
+
+		public bool CheckIfModified(string foreignId, string description);
+	}
+}
